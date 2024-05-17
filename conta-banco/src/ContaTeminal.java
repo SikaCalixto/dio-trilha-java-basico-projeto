983917ca53bb32747b3476bc1953a45a05eda22f
Simone Dias Calixto <simonefulgencio@hotmail.com>
2024-05-17 11:43:36 -0300

    Projeto Criado

--- conta-banco/src/ContaTeminal.java
+++ conta-banco/src/ContaTeminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTeminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+       
+       System.out.println("Por favor, digite o Nome do Usuário !");
+       String usuario = scanner.next();
+       
+       System.out.println("Por favor, digite o número da Conta !");
+       int conta = scanner.nextInt();
+
+       System.out.println("Por favor, digite o número da Agência !");
+       String agencia = scanner.next();
+
+       System.out.println("Por favor, digite o Saldo !");
+       double saldo = scanner.nextDouble();
+
+       System.out.println("Olá " + usuario + " obrigado por criar uma conta em nosso banco, " );
+       System.out.println("sua agência é " + agencia + ", Conta " + conta + ", e seu saldo " + saldo + " já está dispinível para saque");
+      
+     }
+
 }
